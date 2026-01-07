99d0fb1a9e3ca5dae538aa761f00f3e7de6e27d4
Thiago Correa <thiagocdo445@gmail.com>
2026-01-07 11:18:35 -0300

    Adiciona opção para definir tempo de gravação de áudio

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from request_chat import enviar_para_chatgpt
 from response_audio import resposta_em_audio
 
 while input("Pressione Enter para começar a gravação de áudio (ou digite 'sair' para encerrar): ") != 'sair':
-    record_file = record()
+    tempo_gravacao = input("Quanto tempo deseja gravar em segundos? Pressione Enter para 5 segundos ou digite o número de segundos: ")
+    
+    record_file = record(tempo_gravacao or 5)
     transcription = reconhecer_fala(record_file)
     chatgpt_response = enviar_para_chatgpt(transcription)
     response_audio = resposta_em_audio(chatgpt_response)
