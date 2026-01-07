0d06d72ba75b262fd427e0633af7dd6af1a70ce4
Thiago Correa <thiagocdo445@gmail.com>
2026-01-07 11:12:37 -0300

    Adiciona a possibilidade do usuario continuar utilizando o sistema.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from reconhecer_fala import reconhecer_fala
 from request_chat import enviar_para_chatgpt
 from response_audio import resposta_em_audio
 
-record_file = record()
-transcription = reconhecer_fala(record_file)
-chatgpt_response = enviar_para_chatgpt(transcription)
-response_audio = resposta_em_audio(chatgpt_response)
+while input("Pressione Enter para começar a gravação de áudio (ou digite 'sair' para encerrar): ") != 'sair':
+    record_file = record()
+    transcription = reconhecer_fala(record_file)
+    chatgpt_response = enviar_para_chatgpt(transcription)
+    response_audio = resposta_em_audio(chatgpt_response)
 
-display(Audio(response_audio, autoplay=True))
+    display(Audio(response_audio, autoplay=True))
